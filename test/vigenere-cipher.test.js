4bb951e8600d70c480719ff8b4f59e91320ee348
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-03-29 20:59:06 +0300

    feat: add VigenerCipheringMachine

--- test/vigenere-cipher.test.js
+++ test/vigenere-cipher.test.js
@@ -56,7 +56,7 @@ describe('Vigenere cipher', () => {
         });
         
         it.optional('base encryption', () => {
-            assert.equal(directMachine.encrypt('attack at dawn!', 'alphonse'), 'AEIHQX SX DLLU!');
+            assert.equal(directMachine.encrypt('attack at dawn!', 'alphonse'), 'AEIHQX SX DLLU!');                                               
             assert.equal(directMachine.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey'), 'PFLWTJP WQ CIOFMYMI: 1, 2, 3, 4.');
             assert.equal(directMachine.encrypt('cryptography', 'verylongkeyword'), 'XVPNECTXKTFU');
             assert.equal(directMachine.encrypt('Samelengthkey', 'Samelengthkey'), 'KAYIWIAMMOUIW');
