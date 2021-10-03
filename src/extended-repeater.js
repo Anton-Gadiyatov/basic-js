fa4e009eb3078e7c973f14ff2eb95319da4447ba
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-10-03 13:38:54 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,38 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let newArr = [];
+  let repeatTimes = options.repeatTimes;
+  let additionRepeatTimes = options.additionRepeatTimes;
+  if (repeatTimes === undefined) {
+    repeatTimes = 1;
+  }
+  if (additionRepeatTimes === undefined) {
+    additionRepeatTimes = 1;
+  }
+  for (let i = 0; i < repeatTimes; i++) {
+    if (str === null) {
+      newArr.push('null');
+    } else {
+      newArr.push(str.valueOf());
+    }
+    for (let k = 0; k < additionRepeatTimes; k++) {
+      if (options.addition === null) {
+        newArr.push('null');
+      } else if (options.addition !== undefined) {
+        newArr.push(options.addition.valueOf());
+      }
+      options.additionSeparator
+        ? newArr.push(options.additionSeparator)
+        : newArr.push('|');
+
+      if (k === additionRepeatTimes - 1) {
+        newArr.pop();
+      }
+    }
+    options.separator ? newArr.push(options.separator) : newArr.push('+');
+  }
+  newArr.pop();
+  return newArr.join('');
 }
