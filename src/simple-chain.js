cf022388749cf773147c61f3b68f3327e90229a1
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-10-03 15:41:19 +0300

    feat: solve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,27 +2,38 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 export default {
+  temporalArr: [],
+  chain: '',
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.temporalArr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.temporalArr.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      this.temporalArr.length < position ||
+      typeof position !== 'number' ||
+      position < 1
+    ) {
+      this.temporalArr = [];
+      throw new Error("You can't remove incorrect link!");
+    } else {
+      this.temporalArr.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.temporalArr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    this.chain = this.temporalArr.join('~~');
+    this.temporalArr = [];
+    return this.chain;
+  },
 };
