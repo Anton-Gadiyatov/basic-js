b5034d6c235268a4a5c5eebb32ba6a2d62a4996d
Anton Gadiyatov <gadiyatov.an@gmail.com>
2021-10-03 15:27:42 +0300

    feat: solve mine-sweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,157 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper(matrix) {
+  const columns = matrix[0].length;
+  const rows = matrix.length;
+  const newMatrix = [];
+
+  for (let i = 0; i < rows; i++) {
+    newMatrix[i] = new Array(columns).fill(0);
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      let counter = 0;
+      if (i === 0) {
+        if (j === 0) {
+          if (matrix[i + 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j + 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j + 1] === true) {
+            counter += 1;
+          }
+        } else if (j > 0 && j < columns) {
+          if (matrix[i][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j + 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j + 1] === true) {
+            counter += 1;
+          }
+        } else if (j === columns) {
+          if (matrix[i][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j] === true) {
+            counter += 1;
+          }
+        }
+      } else if (i === rows - 1) {
+        if (j === 0) {
+          if (matrix[i - 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i - 1][j + 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j + 1] === true) {
+            counter += 1;
+          }
+        } else if (j > 0 && j < columns) {
+          if (matrix[i - 1][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i - 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i - 1][j + 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j + 1] === true) {
+            counter += 1;
+          }
+        } else if (j === columns) {
+          if (matrix[i - 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i - 1][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j - 1] === true) {
+            counter += 1;
+          }
+        }
+      } else if (i > 0 && i < rows) {
+        if (j === 0) {
+          if (matrix[i - 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i - 1][j + 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j + 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j + 1] === true) {
+            counter += 1;
+          }
+        } else if (j > 0 && j < columns) {
+          if (matrix[i - 1][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i - 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i - 1][j + 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j + 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j + 1] === true) {
+            counter += 1;
+          }
+        } else if (j === columns) {
+          if (matrix[i - 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i - 1][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i][j - 1] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j] === true) {
+            counter += 1;
+          }
+          if (matrix[i + 1][j - 1] === true) {
+            counter += 1;
+          }
+        }
+      }
+
+      newMatrix[i][j] = counter;
+    }
+  }
+  return newMatrix;
 }
